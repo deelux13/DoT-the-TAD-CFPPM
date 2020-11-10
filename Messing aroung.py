5206c174d0020397425d749cf5b8f54a6cd6b9b2
Jared91 <jred.lotz@gmail.com>
2020-11-10 13:11:37 -0700

    Cleaning up extra

--- Messing aroung.py
+++ Messing aroung.py
@@ -9,28 +9,4 @@ def main():
             thd.Trigger(Process)
 
 
-def UBQz():
-    #if __name__ == "__main__":
-    test = True
-
-    while test:
-        ubqs = LZ.pyautogui.prompt(text='How many UBQs?')
-        if ubqs is None:
-            test = False
-            break
-        try:
-            ubqs = int(ubqs)
-            test = False
-        except ValueError:
-            LZ.pyautogui.alert(text="Not a number")
-         ##### THIS needs a failsafe or a
-    # Gets the number of UBQ loops to do
-
-    collect = LZ.pyautogui.confirm(text='Collect coins and do 5 min productions after?', buttons=['Yes', 'No'])
-
-
-
-    LZ.UBQ().go(ubqs)
-    if collect == 'Yes':
-        main()
 
